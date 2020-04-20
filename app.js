b50d321a6d8fca8fe59759814d87c9ada872e0a5
kinslayere <kinslayer.e@gmail.com>
2020-04-20 19:20:57 -0300

    add html content serving

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
+const fs = require('fs');
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+var content = fs.readFileSync('index.html');
+
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader('Content-Type', 'text/html');
+  res.end(content);
 });
 
 server.listen(port, hostname, () => {
